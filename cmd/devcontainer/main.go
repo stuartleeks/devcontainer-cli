5856cf12271b5f5f6139bd51048d97c7d81e0274
Stuart Leeks <stuart@leeks.net>
2020-05-18 06:43:47 +0100

    Extract method, tweak output

--- cmd/devcontainer/main.go
+++ cmd/devcontainer/main.go
@@ -21,45 +21,7 @@ func main() {
 		Short: "List devcontainers",
 		Long:  "Lists devcontainers that are currently running",
 		Run: func(cmd *cobra.Command, args []string) {
-			if listIncludeContainerNames && listVerbose {
-				fmt.Println("Can't use both verbose and include-container-names")
-				os.Exit(1)
-			}
-			devcontainers, err := devcontainers.ListDevcontainers()
-			if err != nil {
-				fmt.Printf("Error: %v", err)
-				os.Exit(1)
-			}
-			if listVerbose {
-				sort.Slice(devcontainers, func(i, j int) bool { return devcontainers[i].DevcontainerName < devcontainers[j].DevcontainerName })
-
-				w := new(tabwriter.Writer)
-				// minwidth, tabwidth, padding, padchar, flags
-				w.Init(os.Stdout, 8, 8, 0, '\t', 0)
-				defer w.Flush()
-
-				fmt.Fprintf(w, "\n %s\t%s\t", "Devcontainer name", "Container name")
-				fmt.Fprintf(w, "\n %s\t%s\t", "----", "----")
-
-				for _, devcontainer := range devcontainers {
-					fmt.Fprintf(w, "\n %s\t%s\t", devcontainer.DevcontainerName, devcontainer.ContainerName)
-				}
-				fmt.Fprintln(w)
-				return
-			}
-			names := []string{}
-			for _, devcontainer := range devcontainers {
-				names = append(names, devcontainer.DevcontainerName)
-			}
-			if listIncludeContainerNames {
-				for _, devcontainer := range devcontainers {
-					names = append(names, devcontainer.ContainerName)
-				}
-			}
-			sort.Strings(names)
-			for _, name := range names {
-				fmt.Println(name)
-			}
+			runListCommand(cmd, args, listIncludeContainerNames, listVerbose)
 		},
 	}
 	cmdList.Flags().BoolVar(&listIncludeContainerNames, "include-container-names", false, "Also include container names in the list")
@@ -67,4 +29,45 @@ func main() {
 
 	rootCmd.AddCommand(cmdList)
 	rootCmd.Execute()
+}
+
+func runListCommand(cmd *cobra.Command, args []string, listIncludeContainerNames bool, listVerbose bool) {
+	if listIncludeContainerNames && listVerbose {
+		fmt.Println("Can't use both verbose and include-container-names")
+		os.Exit(1)
+	}
+	devcontainers, err := devcontainers.ListDevcontainers()
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+	if listVerbose {
+		sort.Slice(devcontainers, func(i, j int) bool { return devcontainers[i].DevcontainerName < devcontainers[j].DevcontainerName })
+
+		w := new(tabwriter.Writer)
+		// minwidth, tabwidth, padding, padchar, flags
+		w.Init(os.Stdout, 8, 8, 0, '\t', 0)
+		defer w.Flush()
+
+		fmt.Fprintf(w, "%s\t%s\n", "DEVCONTAINER NAME", "CONTAINER NAME")
+		fmt.Fprintf(w, "%s\t%s\n", "-----------------", "--------------")
+
+		for _, devcontainer := range devcontainers {
+			fmt.Fprintf(w, "%s\t%s\n", devcontainer.DevcontainerName, devcontainer.ContainerName)
+		}
+		return
+	}
+	names := []string{}
+	for _, devcontainer := range devcontainers {
+		names = append(names, devcontainer.DevcontainerName)
+	}
+	if listIncludeContainerNames {
+		for _, devcontainer := range devcontainers {
+			names = append(names, devcontainer.ContainerName)
+		}
+	}
+	sort.Strings(names)
+	for _, name := range names {
+		fmt.Println(name)
+	}
 }
