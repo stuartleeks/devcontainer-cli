355225b58b7e1cdae1959b4a5e0d5bed4695e86c
Stuart Leeks <stuart@leeks.net>
2020-05-21 21:46:04 +0100

    Add release script

---
+++ .devcontainer/scripts/ci_release.sh
@@ -0,0 +1,28 @@
+set -e
+
+# Fail if build number not set
+if [ -z "$BUILD_NUMBER" ]; then
+    echo "Env var 'BUILD_NUMBER' must be set for this script to work correctly"
+    exit 1
+fi
+
+# If running inside CI login to docker
+if [ -z ${IS_CI} ]; then
+  echo "Not running in CI, skipping CI setup"
+else
+  if [ -z $IS_PR ] && [[ $BRANCH == "refs/heads/master" ]]; then
+    echo "On master setting PUBLISH=true"
+    export PUBLISH=true
+  else
+    echo "Skipping publish as is from PR: $PR_NUMBER or not 'refs/heads/master' BRANCH: $BRANCH"
+  fi
+fi
+
+
+if [ -z ${PUBLISH} ]; then
+  echo "Running with --skip-publish as PUBLISH not set"
+  goreleaser --skip-publish --rm-dist
+else
+  echo "Publishing release"
+  goreleaser
+fi
--- Makefile
+++ Makefile
@@ -4,8 +4,7 @@ build:
 devcontainer:
 	docker build -f ./.devcontainer/Dockerfile ./.devcontainer -t devcontainer-cli
 
-
-devcontainer-build:
+devcontainer-release:
 ifdef DEVCONTAINER
 	$(error This target can only be run outside of the devcontainer as it mounts files and this fails within a devcontainer. Don't worry all it needs is docker)
 endif
@@ -13,4 +12,4 @@ endif
 		--entrypoint /bin/bash \
 		--workdir "${PWD}" \
 		devcontainer-cli \
-		-c "make build"
+		-c "${PWD}/scripts/ci_release.sh"
