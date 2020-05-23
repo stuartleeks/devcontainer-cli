e3e40b32a19380b26dfe407d1dda68a606fef973
Stuart Leeks <stuart@leeks.net>
2020-05-23 10:19:20 +0100

    support different shells for completion

--- cmd/devcontainer/completion.go
+++ cmd/devcontainer/completion.go
@@ -1,26 +1,49 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
 func createCompleteCommand(rootCmd *cobra.Command) *cobra.Command {
-	return &cobra.Command{
-		Use:   "completion",
+	cmd := &cobra.Command{
+		Use:   "completion SHELL",
 		Short: "Generates bash completion scripts",
 		Long: `To load completion run
 	
-	. <(devcontainer completion)
+	. <(devcontainer completion SHELL)
+
+	Valid values for SHELL are : bash, fish, powershell, zsh
 	
-	To configure your bash shell to load completions for each session add to your bashrc
+	For example, to configure your bash shell to load completions for each session add to your bashrc
 	
 	# ~/.bashrc or ~/.profile
 	. <(devcontainer completion)
 	`,
 		Run: func(cmd *cobra.Command, args []string) {
-			rootCmd.GenBashCompletion(os.Stdout)
+			if len(args) != 1 {
+				cmd.Usage()
+				os.Exit(1)
+			}
+			shell := args[0]
+			switch strings.ToLower(shell) {
+			case "bash":
+				rootCmd.GenBashCompletion(os.Stdout)
+			case "fish":
+				rootCmd.GenFishCompletion(os.Stdout, true)
+			case "powershell":
+				rootCmd.GenPowerShellCompletion(os.Stdout)
+			case "zsh":
+				rootCmd.GenPowerShellCompletion(os.Stdout)
+			default:
+				fmt.Printf("Unsupported SHELL value: '%s'\n", shell)
+				cmd.Usage()
+				os.Exit(1)
+			}
 		},
 	}
+	return cmd
 }
